0e3a47894bae1cba9e73ed1e2f912734ffdd90c8
Jose García <jangarstud@gmail.com>
2020-12-29 07:15:34 +0100

    Documentación clase Ruta completa

--- LDH_Localizador_Electronico/src/logica/Ruta.java
+++ LDH_Localizador_Electronico/src/logica/Ruta.java
@@ -2,27 +2,49 @@ package logica;
 
 import java.util.Random;
 
+/*! Clase complementaria de la lógica que instancia una ruta para el objeto de la casa. */
 public class Ruta {
 
-    public static final int COTA_DE_DESPLAZAMIENTO_X = 100;
-    public static final int COTA_DE_DESPLAZAMIENTO_Y = 45;
+    public static final int COTA_DE_DESPLAZAMIENTO_X = 100; /**< Un entero. La cota de desplazamiento para la posición X */
+    public static final int COTA_DE_DESPLAZAMIENTO_Y = 45; /**< Un entero. La cota de desplazamiento para la posición Y */
 
-    private int pos_actual_x;
-    private int pos_actual_y;
+    private int pos_actual_x; /**< Un entero. El valor que tendrá la posición X. */
+    private int pos_actual_y; /**< Un entero. El valor que tendrá la posición Y. */
 
-    private Casa casa;
+    private Casa casa; /**< Un objeto Casa. La referencia del hogar en el que se encuentra el objeto sobre el que se está simulando la ruta */
 
-
+    /**
+     * Constructor vacío de la clase de la ruta.
+     *
+     * En este constructor vacío se inicializan las posiciones X e Y del objeto a 0.
+     */
     public Ruta(){
         pos_actual_x = 0;
         pos_actual_y = 0;
     }
 
+    /**
+     * Constructor de la clase de la ruta con el entorno en el que se está simulando.
+     *
+     * En este constructor se asigna el valor del hogar que se pasa como parámetro al atributo 'casa' de la ruta.
+     * @param casa El entorno en el que se desea ejecutar la simulación de movimiento.
+     */
     public Ruta(Casa casa){
         this();
         this.casa = casa;
     }
 
+    /**
+     * Método que simula y calcula las posiciones X e Y del objeto para esta ruta.
+     *
+     * En este método se calculan las posiciones X e Y del objeto en base a un rangoX y rangoY que serán las cotas
+     * sobre las que se deseará ejecutar la simulación, de tal modo que se pueda ajustar y reflejar un desplazamiento
+     * del objeto más realista. Para aportar cierta naturalidad a la operación, se dota de aleatoriedad a ésta y se
+     * distingue entre 4 casos de ejecución: incrementar X y decrementar Y; decrementar X e incrementar Y;
+     * decrementar X e Y; incrementar X e Y.
+     * @param rangoX Un entero. El rango en el que se desea incrementar/decrementar el valor de X.
+     * @param rangoY Un entero. El rango en el que se desea incrementar/decrementar el valor de Y.
+     */
     public void calcular_pos_actual(int rangoX, int rangoY){
         Random ran1 = new Random();
         Random ran2 = new Random();
@@ -63,10 +85,22 @@ public class Ruta {
         pos_actual_y = valor_Y;
     }
 
+    /**
+     * Getter de la posición X de la ruta.
+     *
+     * Este método devuelve el valor que tiene el atributo 'pos_actual_x' de la ruta.
+     * @return La posición X de la ruta en la que se encuentra el objeto.
+     */
     public int get_pos_x(){
         return pos_actual_x;
     }
 
+    /**
+     * Getter de la posición Y de la ruta.
+     *
+     * Este método devuelve el valor que tiene el atributo 'pos_actual_y' de la ruta.
+     * @return La posición Y de la ruta en la que se encuentra el objeto.
+     */
     public int get_pos_y(){
         return pos_actual_y;
     }
